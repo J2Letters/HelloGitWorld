f147d8e1e26a7441960a391ee9fd6c563e3db107
J2Letters <mastajoffrey@gmail.com>
2016-07-14 08:38:26 -0400

    Trying to commit an unstaged change is useless.
    You have to "add" to the index so that the changes are staged
    
    Signed-off-by: J2Letters <mastajoffrey@gmail.com>

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,15 @@
 
 public class Hello {
+	public String string;
+	
+	public Hello(){
+		this.string = "Hello world";
+	}
+	
+	public Hello(String string){
+		this.string = string;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Coucou monde de git");
 	}
